63ce4f1b625b79fb0374810b2da4063206bc6210
ktan114 <ktaneight@gmail.com>
2021-02-22 23:51:29 -0500

    Initalize call to the github job api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [jobs, updateJobs] = useState([]);
+
+  const retrieveJobs = async () => {
+    const config = {
+    headers: {'Access-Control-Allow-Origin': '*'}
+  };
+    const response = await axios.get("https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=1&search=code", config);
+    const rawJobs = response.data;
+    console.log(rawJobs)
+    updateJobs(rawJobs);
+
+  }
+
+  useEffect(() => {
+      window.onload = retrieveJobs;
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        devjobs
+
+
       </header>
     </div>
   );
